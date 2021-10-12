dd1e66c00a1e5e5d7cb13e30a154034705e651bf
epothos333 <e.aoraha33@gmail.com>
2021-10-12 00:15:50 -0400

    Fix nav link

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -8,9 +8,9 @@ export const MobileMenu = ({handleClick}) => {
     <section className="Mobile-Menu-Container">
           <div className="Flex-Column A-C Mobile-Menu-Top J-S-A">
             <AiOutlineClose onClick={handleClick} className={'Menu-Close'}/>
-            <AnchorLink className="Nav-Link" href="#profession-work" onClick={handleClick}>WORK</AnchorLink>
-            <AnchorLink className="Nav-Link" href="#about" onClick={handleClick}>ABOUT</AnchorLink>
-            <AnchorLink className="Nav-Link" href="#cv" onClick={handleClick}>CV</AnchorLink>
+            <AnchorLink className="Nav-Link-Mobile" href="#profession-work" onClick={handleClick}>WORK</AnchorLink>
+            <AnchorLink className="Nav-Link-Mobile" href="#about" onClick={handleClick}>ABOUT</AnchorLink>
+            <AnchorLink className="Nav-Link-Mobile" href="#cv" onClick={handleClick}>CV</AnchorLink>
             {/* <AnchorLink className="Nav-Link" href="#contact" onClick={handleClick}>CONTACT</AnchorLink> */}
           </div>
           <div className="Mobile-Menu-Bottom"/>
