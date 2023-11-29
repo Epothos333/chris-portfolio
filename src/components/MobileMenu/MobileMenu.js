3b1e4c8f0ac22eb98044b8de4cdeaa7450bd25f4
epothos333 <e.aoraha33@gmail.com>
2023-11-28 20:39:54 -0500

    alt img

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -10,12 +10,11 @@ export const MobileMenu = ({ handleClick }) => {
       <div className="Flex-Column A-C Mobile-Menu-Top J-S-A">
         <div className="Flex-Column A-C J-C Full-Width" style={{height: '100px'}}>
           <AiOutlineClose onClick={handleClick} className={'Menu-Close'} />
-          <img src={logo} className={'Nav-Logo'} alt={'Chris Christopher Consulting Logo'} />
+          <img src={logo} className={'Nav-Logo'} alt={'Navigation Logo'} />
         </div>
         <AnchorLink className="Nav-Link-Mobile" href="#profession-work" onClick={handleClick}>WORK</AnchorLink>
         <AnchorLink className="Nav-Link-Mobile" href="#about" onClick={handleClick}>ABOUT</AnchorLink>
         <AnchorLink className="Nav-Link-Mobile" href="#cv" onClick={handleClick}>CV</AnchorLink>
-        {/* <AnchorLink className="Nav-Link" href="#contact" onClick={handleClick}>CONTACT</AnchorLink> */}
       </div>
       <div className="Mobile-Menu-Bottom" />
     </section>
